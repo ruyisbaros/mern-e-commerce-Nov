bc34a6dac93940297cd6d237383bb1a4fde784cd
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-11-07 19:58:39 +0100

    before deploy last changes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -50,6 +51,13 @@ app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/carts", cartItemRoutes);
 app.use("/api/v1/rates", rateRoutes);
 
+if ((process.env.NODE_ENV = "production")) {
+  app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+//console.log(__dirname);
 const port = process.env.PORT || 8080;
 
 const server = app.listen(port, () => {
