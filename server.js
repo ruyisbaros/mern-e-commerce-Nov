4a660556206ab06341e637727a1e7760e6f8726f
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-11-07 20:31:04 +0100

    heroku fail

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use("/api/v1/carts", cartItemRoutes);
 app.use("/api/v1/rates", rateRoutes);
 
 if ((process.env.NODE_ENV = "production")) {
-  app.use(express.static("../client/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
