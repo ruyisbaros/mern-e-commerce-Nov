407c61d401f9f2f2a955b4f3798d7114b5e09c23
ruyisbaros <ahmet.erdonmez77@gmail.com>
2022-11-08 20:38:08 +0100

    load more func done

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -5,6 +5,7 @@ import { loadingFinish, loadingStart } from "../redux/loadSlicer";
 import axios from "axios";
 import ProductItem from "../components/ProductItem";
 import { getProducts } from "../redux/productsSlicer";
+import infiniteImg from "../images/loading.35b947f5.gif";
 
 const Products = ({ isAdmin }) => {
   const dispatch = useDispatch();
@@ -41,7 +42,7 @@ const Products = ({ isAdmin }) => {
       dispatch(loadingStart());
       const { data } = await axios.get(
         `/api/v1/products/get_all?limit=${
-          page * 9
+          page * 3
         }&sort=${sort}&title[regex]=${search}`
       );
       console.log(data);
@@ -78,8 +79,20 @@ const Products = ({ isAdmin }) => {
     }
   }, [category, sort, search, page]);
 
+  const [loaadImageSeen, setloaadImageSeen] = useState(false);
+
+  const loadingActions = () => {
+    setPage(page + 1);
+    setloaadImageSeen(true);
+    setTimeout(() => {
+      setloaadImageSeen(false);
+      window.scrollTo(0, document.body.scrollHeight);
+    }, 2000);
+  };
+
   return (
     <div className="products">
+      {/* Filter options */}
       <div className="filetrs-main">
         <div className="filter_content">
           <span>Filter:</span>
@@ -97,6 +110,28 @@ const Products = ({ isAdmin }) => {
             ))}
           </select>
         </div>
+
+        <input
+          type="text"
+          placeholder="Type your key word"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        />
+
+        <div className="filter_content">
+          <span>Sort By:</span>
+          <select
+            name="sort"
+            id=""
+            value={sort}
+            onChange={(e) => setSort(e.target.value)}
+          >
+            <option value="">Newest</option>
+            <option value="oldest">Oldest</option>
+            <option value="-price">Price: Highg to Low</option>
+            <option value="price">Price: Low to Highg</option>
+          </select>
+        </div>
       </div>
       <div className="products-sorround">
         {products?.map((product) => (
@@ -108,8 +143,27 @@ const Products = ({ isAdmin }) => {
           />
         ))}
       </div>
+      {loaadImageSeen && (
+        <div className="infinite_loading">
+          <img src={infiniteImg} alt="" />
+        </div>
+      )}
+      <button className="load_more_btn" onClick={loadingActions}>
+        Load More
+      </button>
     </div>
   );
 };
 
 export default Products;
+
+/* 
+ const handleColor = () => {
+    if (window.scrollY >= document.documentElement.clientWidth) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  };
+document.documentElement.clientWidth
+  window.addEventListener("scroll", handleColor); */
